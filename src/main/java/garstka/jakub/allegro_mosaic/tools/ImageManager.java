3b81a72d87d9448cdbfe7c63a2b5efa85dab7ce6
Suqu13 <jgarstka3@gmail.com>
2019-04-20 18:13:10 +0200

    improvements

--- src/main/java/garstka/jakub/allegro_mosaic/tools/ImageManager.java
+++ src/main/java/garstka/jakub/allegro_mosaic/tools/ImageManager.java
@@ -14,7 +14,8 @@ public class ImageManager {
 
     private BufferedImage loadImage(String url) throws IOException {
             return ImageIO.read(new URL(url));
-        }
+    }
+
 
     public BufferedImage createMosaic(List<String> imagesUrls, int maxMosaicWidth, int maxMosaicHeight) throws IOException {
         List<BufferedImage> images = new ArrayList<>();
@@ -22,33 +23,34 @@ public class ImageManager {
             BufferedImage bufferedImage = loadImage(imagesUrl);
             images.add(bufferedImage);
         }
-        int numberOfElementInRow = images.size() < 6 ? 2 : 3;
-        BufferedImage concatImage = concatIntoMosaic(resizeImages(images, numberOfElementInRow, maxMosaicWidth, maxMosaicHeight), numberOfElementInRow);
+
+        int numberOfElementInRow = (int) Math.round(Math.sqrt(images.size()));
+
+        BufferedImage concatImage = concatenateIntoMosaic(resizeImages(images, numberOfElementInRow, maxMosaicWidth, maxMosaicHeight), numberOfElementInRow);
         concatImage = resizeImage(concatImage, maxMosaicWidth, maxMosaicHeight);
         return concatImage;
     }
 
-    private BufferedImage concatIntoMosaic(List<BufferedImage> images, int numberOfElementInRow) {
+
+    private BufferedImage concatenateIntoMosaic(List<BufferedImage> images, int numberOfElementInRow) {
         List<BufferedImage> temporaryImages = new ArrayList<>();
-        int firstRowIndex = 0;
-        // Concatenate vertically images only for full rows
-        while (firstRowIndex < images.size() - numberOfElementInRow) {
-            int lastRowIndex = firstRowIndex + numberOfElementInRow;
-            temporaryImages.add(concatenateVertically(images.subList(firstRowIndex, lastRowIndex)));
-            firstRowIndex += numberOfElementInRow;
+        int rowIndex = 0;
+
+        while (rowIndex < images.size() - numberOfElementInRow) {
+            temporaryImages.add(concatenateHorizontally(images.subList(rowIndex, rowIndex + numberOfElementInRow)));
+            rowIndex += numberOfElementInRow;
         }
-        // If only full rows exist then concatenate horizontally list of temporary images which are after vertical concatenation.
-        // Else concatenate vertically images from not full row and add these to rest of vertically concatenated. After that
-        // also concatenate horizontally list of temporary images.
-        if (images.size() == firstRowIndex) {
-            return concatenateHorizontally(temporaryImages);
+
+        if (images.size() == rowIndex) {
+            return concatenateVertically(temporaryImages);
         } else {
-            temporaryImages.add(concatenateVertically(images.subList(firstRowIndex, images.size())));
-            return concatenateHorizontally(temporaryImages);
+            temporaryImages.add(concatenateHorizontally(images.subList(rowIndex, images.size())));
+            return concatenateVertically(temporaryImages);
         }
     }
 
-    private BufferedImage concatenateHorizontally(List<BufferedImage> images) {
+
+    private BufferedImage concatenateVertically(List<BufferedImage> images) {
         int maxHeight = images.stream().map(BufferedImage::getHeight).reduce(0, Integer::sum);
         
         BufferedImage concatenatedImage = new BufferedImage(images.get(0).getWidth(), maxHeight, BufferedImage.TYPE_INT_RGB);
@@ -62,7 +64,8 @@ public class ImageManager {
         return concatenatedImage;
     }
 
-    private BufferedImage concatenateVertically(List<BufferedImage> images) {
+
+    private BufferedImage concatenateHorizontally(List<BufferedImage> images) {
         int maxWidth = images.stream().map(BufferedImage::getWidth).reduce(0, Integer::sum);
         
         BufferedImage concatenatedImage = new BufferedImage(maxWidth, images.get(0).getHeight(), BufferedImage.TYPE_INT_RGB);
@@ -76,38 +79,38 @@ public class ImageManager {
         return concatenatedImage;
     }
 
+
     private List<BufferedImage> resizeImages(List<BufferedImage> images, int numberOfElementInRow, int maxMosaicWidth, int maxMosaicHeight) {
         int[] size = findRightSize(maxMosaicWidth, maxMosaicHeight, images.size(), numberOfElementInRow);
         return createResizedImagesList(size, images, numberOfElementInRow);
     }
 
+
     private int[] findRightSize(int maxMosaicWidth, int maxMosaicHeight, int numberOfElementsInList, int numberOfElementInRow) {
-        int imageHeight;
-        int imageWidth;
+        int imageHeight = maxMosaicHeight;
+        int imageWidth = maxMosaicWidth;
         int lastRowWidth = maxMosaicWidth;
 
-        int fullRows = (int) Math.floor(numberOfElementsInList / numberOfElementInRow);
-        if (fullRows == 0) {
-            imageHeight = maxMosaicHeight;
-            imageWidth = maxMosaicWidth;
+        int fullRows = numberOfElementsInList / numberOfElementInRow;
+        int numberOfElementInLastRow = numberOfElementsInList % numberOfElementInRow;
+
+        imageWidth /= numberOfElementInRow;
+        if (numberOfElementInLastRow != 0) {
+            imageHeight /= (fullRows + 1);
+            lastRowWidth /= numberOfElementInLastRow;
         } else {
-            imageWidth = maxMosaicWidth / numberOfElementInRow;
-            int numberOfElementInLastRow = numberOfElementsInList % numberOfElementInRow;
-            if (numberOfElementInLastRow != 0) {
-                imageHeight = maxMosaicHeight / (fullRows + 1);
-                lastRowWidth = maxMosaicWidth / numberOfElementInLastRow;
-            } else {
-                imageHeight = maxMosaicHeight / fullRows;
-            }
+            imageHeight /= fullRows;
         }
+
         return new int[]{imageHeight, imageWidth, lastRowWidth};
     }
 
+
     private List<BufferedImage> createResizedImagesList(int[] size, List<BufferedImage> images, int numberOfElementInRow) {
         List<BufferedImage> newImages = new ArrayList<>();
-        int indexesOfElementsInLastRow = images.size() - (images.size() % numberOfElementInRow);
+        int IndexOfLastElementInLastFullRow  = images.size() - (images.size() % numberOfElementInRow);
         for (int i = 0; i < images.size(); i++) {
-                if (i >=  indexesOfElementsInLastRow) {
+                if (i >=  IndexOfLastElementInLastFullRow ) {
                     newImages.add(resizeImage(images.get(i), size[2], size[0]));
                 } else {
                     newImages.add(resizeImage(images.get(i), size[1], size[0]));
@@ -115,6 +118,7 @@ public class ImageManager {
         }
         return newImages;
     }
+
 
     private  BufferedImage resizeImage(BufferedImage image, int width, int height){
         BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
